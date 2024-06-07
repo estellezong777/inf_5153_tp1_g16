eb510fbc906fb7ff279af5219ea89a284227057a
Moyalin1 <116445277+Moyalin1@users.noreply.github.com>
2024-06-08 02:50:56 +0800

    Add files via upload

---
+++ src/EncoderNeutre.java
@@ -0,0 +1,11 @@
+public class EncoderNeutre extends Encoder{
+    @Override
+    public String encoder (String msg){
+        return msg;
+    }
+
+    public String Decode(String msg){
+        return msg;
+    }
+
+}
